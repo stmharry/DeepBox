0ecc805ccd77b86e25b596ac82d8896b6305d2a1
stmharry <harry19930924@gmail.com>
2016-02-19 09:52:12 +0800

    Updated many new things...

--- deepbox.py
+++ deepbox.py
@@ -1,50 +1,101 @@
-import tensorflow as tf
+from __future__ import print_function
 
-from numpy import mean, std, unique, prod
-from numpy import arange
-from numpy import concatenate
+import sys
+import tensorflow as tf
+import numpy as np
 
 _EPSILON = 1e-7
+_EPOCH = 1
+_BATCH_SIZE = 256
+_BAR_LENGTH = 20
+
+TRAIN = 0
+VAL = 1
+TEST = 2
 
 class Dataset(object):
-    def __init__(self, **kwargs):
-        (self.X, self.Y) = kwargs['data']
-        (self.len_train, self.len_val, self.len_test) = kwargs['length']
-
-        if 'normalize' in kwargs:
-            if kwargs['normalize'] == True:
-                self.X = Dataset.normalize(self.X)
-
-        if 'one_hot' in kwargs:
-            if kwargs['one_hot'] == True:
-                (self.Y, self.y0) = Dataset.one_hot(self.Y)
-            else:
-                self.y0 = arange(self.Y.shape[1])
-        
-        self.input_dim = self.X.shape[1:]
-        self.output_dim = self.Y.shape[1:]
-    
     @staticmethod
-    def normalize(X): 
-        return (X - mean(X, 0)) / (std(X, 0) + _EPSILON)
+    def split(X, ind):
+        return [np.split(x, ind) for x in X]
+
+    @staticmethod
+    def normalize(X):
+        _X = np.concatenate(X)
+        m = np.nanmean(_X, 0)
+        s = np.nanstd(_X, 0) + _EPSILON
+        
+        return [(x - m) / s for x in X]
 
     @staticmethod
     def one_hot(Y):
-        y0 = unique(Y)
-        return ((Y[:, None] == y0).astype(float), y0)
+        _Y = np.concatenate(Y)
+        y0 = np.unique(_Y[~np.isnan(_Y)])
+      
+        _Y = []
+        for y in Y:
+            _y = (y[:, None] == y0).astype(float)
+            _y[np.isnan(y), :] = np.NaN
+            _Y += [_y]
 
+        return (_Y, y0)
+
+    @staticmethod
+    def get_sample_num(feed_dict):
+        return max([v.shape[0] for v in feed_dict.values() if isinstance(v, np.ndarray)]) if feed_dict else 0
+
+    @staticmethod
+    def get_batch(feed_dict, batch_size):
+        sample_num = Dataset.get_sample_num(feed_dict)
+        total_batch_num = int(np.ceil(sample_num / batch_size))
+
+        for batch_num in range(total_batch_num):
+            batch = {}
+            min_batch_index = batch_num * batch_size
+            max_batch_index = min((batch_num + 1) * batch_size, sample_num)
+
+            for (key, value) in feed_dict.iteritems():
+                if isinstance(value, np.ndarray):
+                    batch[key] = value[min_batch_index:max_batch_index]
+                else:
+                    batch[key] = value
+            yield (batch, batch_num, min_batch_index, max_batch_index)
+
+    @staticmethod
+    def permute(feed_dict):
+        perm = np.random.permutation(Dataset.get_sample_num(feed_dict))
+        return dict([(key, value[perm]) if isinstance(value, np.ndarray) else (key, value) for (key, value) in feed_dict.iteritems()])
+        
 class Model(object):
-    def __init__(self):
-        self.inputs = []
-        self.outputs = []
+    @staticmethod
+    def print_report(phase, epoch_num, total_epoch_num, progress, keys, values, end_token):
+        phase_str = {TRAIN:'[TRAINING]', VAL:'[VALIDATION]', TEST:'[TESTING]'}
+        progress_length = int(progress / 100 * _BAR_LENGTH)
+        neg_progress_length = _BAR_LENGTH - progress_length
+
+        print('%-12s Epoch: %2d/%2d  Progress: [%s%s]  ' %(phase_str[phase], epoch_num, total_epoch_num, '=' * progress_length, '-' * neg_progress_length), end='')
+        if (len(keys) != 0) and (len(values) != 0):
+            print('Value: ', end='')
+            for (k, v) in zip(keys, values):
+                print('%s = %.4f  ' %(k, v), end='')
+        print('\033[K', end=end_token)
+        sys.stdout.flush()
 
     @staticmethod
     def get_shape(x):
-        return x.get_shape().as_list()[1:]
+        return tuple([int(s) if s is not None else None for s in x.get_shape().as_list()])
 
     @staticmethod
+    def get_batch_shape(x):
+        return tuple([int(s) if s is not None else None for s in x.get_shape().as_list()[1:]])
+    
+    @staticmethod
     def get_size(x):
-        return prod(Model.get_shape(x))
+        return np.prod(Model.get_batch_shape(x))
+
+    @staticmethod
+    def full_shape(shape):
+        assert len(shape) == 2
+        return (1,) + shape + (1,)
 
     @staticmethod
     def weight(shape):
@@ -52,25 +103,141 @@ class Model(object):
 
     @staticmethod
     def bias(shape):
-        return tf.Variable(tf.constant(0.1, shape=shape))
+        #return tf.Variable(tf.constant(0.1, shape=shape))
+        return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
     @staticmethod
-    def dense(x, shape):
-        return tf.matmul(x, Model.weight(shape)) + Model.bias(shape[-1:])
+    def dense(x, output_dim):
+        input_shape = Model.get_batch_shape(x)
+        assert len(input_shape) == 1
+        return tf.matmul(x, Model.weight((input_shape[0], output_dim))) + Model.bias((output_dim,))
+
+    @staticmethod
+    def dense_relu_drop(x, d, output_dim):
+        return tf.nn.dropout(tf.nn.relu6(Model.dense(x, output_dim)), d)
+
+    @staticmethod
+    def dense_sequential(x, d, arch):
+        h = x
+        for output_dim in arch:
+            h = Model.dense_relu_drop(h, d, output_dim)
+        return h
+
+    @staticmethod
+    def flatten(x):
+        return tf.reshape(x, (-1, Model.get_size(x)))
     
     @staticmethod
-    def max_pool(x, shape, strides, padding):
-        return tf.nn.max_pool(x, shape, strides, padding)
+    def max_pool(x, shape, strides=(), padding='VALID'):
+        input_shape = Model.get_batch_shape(x)
+        assert len(input_shape) == 3
+        if not strides:
+            strides = shape
+        return tf.nn.max_pool(x, Model.full_shape(shape), Model.full_shape(strides), padding)
 
     @staticmethod
-    def conv2d(x, shape, strides, padding):
-        return tf.nn.conv2d(x, Model.weight(shape), strides, padding) + Model.bias(shape[-1:])
+    def conv2d(x, num_kernel, shape, strides=(), padding='VALID'):
+        input_shape = Model.get_batch_shape(x)
+        assert len(input_shape) == 3
+        if not strides:
+            strides = shape
+        return tf.nn.conv2d(x, Model.weight(shape + (input_shape[2], num_kernel)), Model.full_shape(strides), padding) + Model.bias((num_kernel,))
 
-    def set_inputs(inputs):
-        self.inputs = inputs
+    @staticmethod
+    def embedding(x, shape):
+        input_shape = Model.get_batch_shape(x)
+        assert len(input_shape) == 1
+        with tf.device("/cpu:0"):
+            return tf.reshape(tf.nn.embedding_lookup(Model.weight(shape), x), (-1, shape[1]))
 
-    def set_outputs(outputs):
-        self.outputs = outputs
+    #
+    def __init__(self):
+        self.saver = tf.train.Saver()
+        self.sess = tf.Session()
 
-    def set_init()
-        # sess
+        self.val_flag = False
+        self.args = [{
+            'inputs': {}, 
+            'outputs': {},
+            'updates': {},
+            'kwargs': {'epoch':_EPOCH, 'batch_size':_BATCH_SIZE}} for _ in xrange(3)]
+        self.output_values = [[] for _ in xrange(3)]
+
+    def init(self):
+        self.sess.run(tf.initialize_all_variables())
+
+    def set(self, phase, inputs=None, outputs=None, updates=None, **kwargs):
+        if phase == VAL:
+            self.val_flag = True
+        for (var, attr) in zip([inputs, outputs, updates], ['inputs', 'outputs', 'updates']):
+            if var is not None:
+                self.args[phase][attr] = var
+        self.args[phase]['kwargs'].update(kwargs)
+
+    #
+
+    def train(self, **kwargs):
+        self.set(TRAIN, **kwargs)
+        return self.feed(TRAIN)
+
+    def test(self, **kwargs):
+        self.set(TEST, **kwargs)
+        return self.feed(TEST)
+
+    #
+
+    def feed(self, phase):
+        args = self.args[phase]
+        (inputs, outputs, updates, kwargs) = (args['inputs'], args['outputs'], args['updates'], args['kwargs'])
+        (total_epoch_num, batch_size) = (kwargs['epoch'], kwargs['batch_size'])
+
+        sample_num = Dataset.get_sample_num(inputs)
+        batch_size = sample_num if batch_size == -1 else batch_size
+        total_batch_num = int(np.ceil(sample_num / batch_size))
+        self.output_values[phase] = []
+            
+        key_shape = [Model.get_shape(key) for key in outputs.keys()]
+        key_form = [bool(shape) and ((shape[0] is None) or (shape[0] == batch_size)) for shape in key_shape]
+        key_show = [not form and value is not None for (form, value) in zip(key_form, outputs.values())]
+
+        for epoch_num in xrange(total_epoch_num):
+            if phase == TRAIN:
+                inputs = Dataset.permute(inputs)
+
+            value_epoch = dict([
+                (key, np.zeros((sample_num,) + shape[1:]) if form else np.zeros((total_batch_num,) + shape))
+                for (key, shape, form) in zip(outputs.keys(), key_shape, key_form)])
+
+            for (batch, batch_num, min_batch_index, max_batch_index) in Dataset.get_batch(inputs, batch_size):
+                value_batch = self.sess.run(outputs.keys() + updates.keys(), feed_dict=batch)[:len(outputs)]
+               
+                for (key, value, form) in zip(outputs.keys(), value_batch, key_form):
+                    if form:
+                        value_epoch[key][min_batch_index:max_batch_index] = value
+                    else:
+                        value_epoch[key][batch_num] = value
+
+                Model.print_report(
+                    phase,
+                    epoch_num + 1, 
+                    total_epoch_num, 
+                    (batch_num + 1.) / total_batch_num * 100, 
+                    [value for (show, value) in zip(key_show, outputs.values()) if show],
+                    [np.sum(value) / (batch_num + 1) for (show, value) in zip(key_show, value_epoch.values()) if show],
+                    '\r')
+
+            self.output_values[phase] += [value_epoch]
+            print('')
+            if (phase == TRAIN) and self.val_flag:
+                self.feed(VAL)
+
+    def save(self, path, step=None):
+        if step is None:
+            self.saver.save(self.sess, path)
+        else:
+            self.saver.save(self.sess, path, global_step=step)
+
+    def load(self, path):
+        state = tf.train.get_checkpoint_state(path)
+        if state and state.model_checkpoint_path:
+            self.saver.restore(sess, state.model_checkpoint_path)
--- main.py
+++ main.py
@@ -1,47 +1,83 @@
 import tensorflow as tf
 import deepbox as db
+import numpy as np
+import os
 
 from scipy.io import loadmat
-from numpy import concatenate
 from keras.datasets import mnist
 
-(X_train, y_train), (X_test, y_test)) = mnist.load_data()
+def get_path(issave):
+    isload = not issave
+    root = 'temp/'
+    i = 0
+    while True:
+        path = root + str(i) + '/model'
+        if os.path.isfile(path):
+            i += 1
+            if isload:
+                yield path
+        else:
+            directory = root + str(i)
+            if issave and not os.path.isdir(directory):
+                os.mkdir(directory)
+            break
+    yield path
 
-dataset = db.Dataset(
-    data=[concatenate([X_train, X_test])], concatenate([y_train, y_test])[None, :]], 
-    length=[y_train.size, 0, y_test.size],
-    normalize=True, one_hot=True)
+def get_save_path():
+    return [p for p in get_path(True)][0]
 
-#
+def get_load_path():
+    for p in get_path(False):
+        yield p
+
+if 'dataset' not in locals():
+    ((X_train, y_train), (X_test, y_test)) = mnist.load_data()
+
+    X_train = X_train[0:1000]
+    y_train = y_train[0:1000]
+    X_test = X_test[0:1000]
+    y_test = y_test[0:1000]
+
+    dev_ratio = 0.8
+    len_dev = int(dev_ratio * X_train.shape[0])
+    
+    (X_dev, X_val) = np.split(X_train, [len_dev])
+    (y_dev, y_val) = np.split(y_train, [len_dev])
+
+    (X_dev, X_val, X_test) = db.Dataset.normalize([X_dev, X_val, X_test])
+    ((y_dev, y_val, y_test), y0) = db.Dataset.one_hot([y_dev, y_val, y_test])
+
+if 'model' not in locals():
+    x = tf.placeholder('float', shape=[None, 28, 28])
+    y = tf.placeholder('float', shape=[None, 10])
+    d = tf.placeholder('float')
+
+    h = tf.reshape(x, (-1, 28, 28, 1)) 
+    h = db.Model.conv2d(h, 32, (5, 5), (1, 1), 'SAME')
+    h = tf.nn.relu6(h)
+    h = db.Model.max_pool(h, (2, 2), (2, 2), 'SAME')
+
+    h = db.Model.conv2d(h, 64, (5, 5), (1, 1), 'SAME')
+    h = tf.nn.relu6(h)
+    h = db.Model.max_pool(h, (2, 2), (2, 2), 'SAME')
+
+    h = tf.reshape(h, [-1, db.Model.get_size(h)])
+    h = db.Model.dense(h, 1024)
+    h = tf.nn.relu6(h)
+    h = tf.nn.dropout(h, d)
+    h = db.Model.dense(h, 10)
+    yp = tf.nn.softmax(h)
+
+    loss = -tf.reduce_mean(y * tf.log(yp))
+    correct = tf.equal(tf.argmax(yp, 1), tf.argmax(y, 1))
+    acc = tf.reduce_mean(tf.cast(correct, 'float'))
+
+    train = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
 model = db.Model()
+model.init()
 
-x = tf.placeholder('float', shape=(None,) + dataset.input_dim)
-y = tf.placeholder('float', shape=(None,) + dataset.output_dim)
-d = tf.placeholder('float')
+model.set(db.TRAIN, {x:X_dev, y:y_dev, d:0.5}, {loss:'loss', acc:'acc'}, {train:None}, epoch=1, batch_size=128)
+model.set(db.VAL, {x:X_val, y:y_val, d:0.5}, {loss:'loss', acc:'acc'})
+model.set(db.TEST, {x:X_test, y:y_test, d:1.0}, {yp:None})
 
-h = tf.reshape(x, (-1,) + dataset.dim + (1,)) 
-h = db.Model.conv2d(h, (5, 5, 1, 32), (1, 1, 1, 1), 'SAME')
-h = tf.nn.relu(h)
-h = db.Model.max_pool(h, (1, 2, 2, 1), (1, 2, 2, 1), 'SAME')
-
-h = db.Model.conv2d(h, (5, 5, 32, 64), (1, 1, 1, 1), 'SAME')
-h = tf.nn.relu(h)
-h = db.Model.max_pool(h, (1, 2, 2, 1), (1, 2, 2, 1), 'SAME')
-
-h = tf.reshape(h, (-1,) + db.Model.get_size(h))
-h = db.Model.dense(h, (db.Model.get_size(h), 1024))
-h = tf.nn.relu(h)
-h = tf.nn.dropout(h, d)
-
-h = db.Model.dense(h, (1024, 10))
-yp = tf.nn.softmax(h)
-
-ce = -tf.reduce_sum(y * tf.log(yp))
-train = tf.train.AdamOptimizer(1e-3).minimize(ce)
-
-model.set_inputs([x, d])
-model.set_outputs([y])
-
-model.set_init()
-model.set_train()
-model.save()
